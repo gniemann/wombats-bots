f0c6ab49e0ff681654091c7e95e21783cca1077f
Greg Niemann <greg.niemann@willowtreeapps.com>
2017-08-03 13:29:56 -0400

    Make all random choices

--- airborne_wombat.py
+++ airborne_wombat.py
@@ -1,9 +1,23 @@
 
 def wombat(state, time_left):
-    action = 'turn'
-    metadata = {'direction': 'right'}
-    state = {'hello': 'world'}
+    import random
+    actions = ['turn', 'move', 'shoot', 'smoke']
+    turn_directions = ['right', 'left', 'about-face']
+    smoke_directions = ['forward', 'behind', 'left', 'right']
 
+
+    action = None
+    metadata = {}
+    state = {}
+
+    action = random.choice(actions)
+
+    if action is 'turn':
+        direction = random.choice(turn_directions)
+        metadata['direction'] = direction
+    elif action is 'smoke':
+        direction = random.choice(smoke_directions)
+        metadata['direction'] = direction
 
     command = { 'command': {
         'action': action,
