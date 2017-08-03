08bab70c7e838be17c7af93b984844082c94f1da
Greg Niemann <greg.niemann@willowtreeapps.com>
2017-08-03 14:40:15 -0400

    Add bot logic

--- airborne_wombat.py
+++ airborne_wombat.py
@@ -2,29 +2,101 @@
 def wombat(state, time_left):
     import random
     from collections import namedtuple
+
     Coordinates = namedtuple('Coordinates', ('row', 'col'))
+    SHOOT = 'shoot'
+    MOVE = 'move'
+    TURN = 'turn'
+    SMOKE = 'smoke'
 
     actions = ['turn', 'move', 'shoot', 'smoke']
     turn_directions = ['right', 'left', 'about-face']
     smoke_directions = ['forward', 'behind', 'left', 'right']
+    orientations = ['n', 's', 'e', 'w']
+
+    #functions
+    def check_for_enemy(arena, row, col):
+        contents = arena[row][col]
+        other_wombat = contents.get('type', None)
+        if other_wombat == 'zakano' or other_wombat == 'wombat':
+            return True
+        else:
+            return False
+
+    def whats_in_front(arena, coords, orientation):
+        row = coords.row
+        col = coords.col
+        if orientation == 'n':
+            row = row - 1
+        elif orientation == 's':
+            row = row + 1
+        elif orientation == 'e':
+            col = col + 1
+        else:
+            col = col - 1
+
+        if row < 0 or row > len(arena) or col < 0 or col > len(arena):
+            return None
+
+        contents = arena[row][col]
+        return contents.get('type', None)
 
     arena = state['arena']
     local_coords = Coordinates(*[int(x) for x in state['local-coords']])
     global_coords = Coordinates(*[int(x) for x in state['global-coords']])
     global_dimensions = Coordinates(*[int(x) for x in state['global-dimensions']])
-    hp = arena[local_coords.row][local_coords.col]['contents']['hp']
 
+    me = arena[local_coords.row][local_coords.col]['contents']
+    hp = me['hp']
+    orientation = me['orientation']
     action = None
     metadata = {}
 
-    saved_state = state.get('saved-state', None)
-
-    if saved_state and saved_state.get('prev_action', None) == 'turn':
-        action = 'shoot'
+    # first see if we can blaze something
+    if orientation == 'n':
+        # look all rows above us
+        for row in range(0, local_coords.row):
+            if check_for_enemy(arena, row, local_coords.col):
+                action = SHOOT
+                break
+    elif orientation == 's':
+        #all rows below
+        for row in range(local_coords.row + 1, len(arena)):
+            if check_for_enemy(arena, row, local_coords.col):
+                action = SHOOT
+                break
+    elif orientation == 'e':
+        #all cols to right
+        for col in range(local_coords.col + 1, len(arena)):
+            if check_for_enemy(arena, local_coords.row, col):
+                action = SHOOT
+                break
     else:
-        action = 'turn'
-        direction = random.choice(turn_directions)
-        metadata['direction'] = direction
+        #all cols left
+        for col in range(0, local_coords.col):
+            if check_for_enemy(arena, local_coords.row, col):
+                action = SHOOT
+                break
+
+    if not action:
+        in_front = whats_in_front(arena, local_coords, orientation)
+        if in_front == 'food':
+            action = MOVE
+        elif in_front != 'open':
+            #turn somewhere
+            action = 'turn'
+            direction = random.choice(turn_directions)
+            metadata['direction'] = direction
+        else:
+            move_or_turn = [MOVE, TURN]
+            action = random.choice(move_or_turn)
+            if action == MOVE:
+                direction = random.choice(turn_directions)
+                metadata['direction'] = direction
+
+
+        saved_state = state.get('saved-state', None)
+
 
     save_state = {'coords': local_coords,
                   'hp': hp,
