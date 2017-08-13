deaa7be09f2c93f4d949b87fa712c6f8ec9cc70b
Greg Niemann <greg.niemann@willowtreeapps.com>
2017-08-13 12:37:08 -0400

    Refactor with Enum

--- airborne_wombat.py
+++ airborne_wombat.py
@@ -3,19 +3,56 @@ def wombat(state, time_left):
     import random
     import time
     from collections import namedtuple
+    from enum import Enum
 
-    random.seed(time.time())
+    class Actions(Enum):
+        shoot = 'shoot'
+        move = 'move'
+        turn = 'turn'
+        smoke = 'smoke'
 
-    Coordinates = namedtuple('Coordinates', ('row', 'col'))
-    SHOOT = 'shoot'
-    MOVE = 'move'
-    TURN = 'turn'
-    SMOKE = 'smoke'
-    FOOD = 'food'
+        def __str__(self):
+            return self.value
 
-    actions = ['turn', 'move', 'shoot', 'smoke']
-    turn_directions = ['right', 'left', 'about-face']
-    smoke_directions = ['forward', 'behind', 'left', 'right']
+    class TurnDirections(Enum):
+        right = 'right'
+        left = 'left'
+        behind = 'about-face'
+        forward = 'forward'
+
+        def __str__(self):
+            return self.value
+
+        @classmethod
+        def random_direction(cls):
+            return random.choice(list(cls))
+
+    class SmokeDirections(Enum):
+        forward = 'forward'
+        behind = 'behind'
+        left = 'left'
+        right = 'right'
+
+        def __str__(self):
+            return self.value
+
+        @classmethod
+        def random_direction(cls):
+            return random.choice(list(cls))
+
+    class Items(Enum):
+        food = 'food'
+        wood_barrier = 'wood-barrier'
+        open = 'open'
+
+        def __str__(self):
+            return self.value
+
+        def __eq__(self, other):
+            return str(self) == str(other)
+
+    Point = namedtuple('Point', ('row', 'col'))
+
     orientations = {'n': 0,
                     'e': 1,
                     's': 2,
@@ -33,98 +70,123 @@ def wombat(state, time_left):
                 get_contents(arena, row + 1, col),
                 get_contents(arena, row, col - 1)]
 
+
     def check_for_enemy(arena, row, col):
         contents = get_contents(arena, row, col)
-        return contents == 'zakano' or contents == 'wombat' or contents == 'wood-barrier'
+        return contents == 'zakano' or contents == 'wombat'
 
+    def visible_enemies(arena, coords):
+        enemy_coords = []
+        for row in range(len(arena)):
+            col = coords.col
+            if row != coords.row and check_for_enemy(arena, row, col):
+                enemy_coords.append(Point(row, col))
+        for col in range(len(arena)):
+            row = coords.row
+            if col != coords.col and check_for_enemy(arena, row, col):
+                enemy_coords.append(Point(row, col))
 
+        return enemy_coords
+
+    # action commands
+    def command(action, metadata):
+        if not metadata:
+            metadata = {}
+        return {
+            'command': {
+                'action': action,
+                'metadata': metadata
+            }
+        }
+
+    def shoot():
+        return command(Actions.shoot)
+
+    def turn(direction):
+        return command(Actions.turn, direction)
+
+    def smoke(direction):
+        return command(Actions.smoke, direction)
+
+    def move():
+        return command(Actions.move)
+
+    def commandToReturn(action, orientation, in_front, direction=None):
+        command = ''
+        if action is Actions.shoot:
+            command = shoot()
+        elif action is Actions.move:
+            command = move()
+        elif action is Actions.turn:
+            command = turn(direction)
+        elif action is Actions.smoke:
+            command = smoke(direction)
+
+        save_state = {
+            'orientation': orientation,
+            'in_front': in_front,
+            'prev_action': action
+        }
+
+        return {
+            'command': command,
+            'state': save_state
+        }
+
+    random.seed(time.time())
     arena = state['arena']
-    local_coords = Coordinates(*[int(x) for x in state['local-coords']])
-    global_coords = Coordinates(*[int(x) for x in state['global-coords']])
-    global_dimensions = Coordinates(*[int(x) for x in state['global-dimensions']])
+    local_coords = Point(*[int(x) for x in state['local-coords']])
+    global_coords = Point(*[int(x) for x in state['global-coords']])
+    global_dimensions = Point(*[int(x) for x in state['global-dimensions']])
 
     me = arena[local_coords.row][local_coords.col]['contents']
     hp = me['hp']
     orientation = me['orientation']
-    action = None
     metadata = {}
-    direction = None
     next_to = adj_items(arena, local_coords)
     current_orientation = orientations[orientation]
     in_front = next_to[current_orientation]
 
+    enemies = visible_enemies(arena, local_coords)
+
     # first see if we can blaze something
-    if orientation == 'n':
-        # look all rows above us
-        for row in range(0, local_coords.row):
-            if check_for_enemy(arena, row, local_coords.col):
-                action = SHOOT
-                break
-    elif orientation == 's':
-        #all rows below
-        for row in range(local_coords.row + 1, len(arena)):
-            if check_for_enemy(arena, row, local_coords.col):
-                action = SHOOT
-                break
-    elif orientation == 'e':
-        #all cols to right
-        for col in range(local_coords.col + 1, len(arena)):
-            if check_for_enemy(arena, local_coords.row, col):
-                action = SHOOT
-                break
-    else:
-        #all cols left
-        for col in range(0, local_coords.col):
-            if check_for_enemy(arena, local_coords.row, col):
-                action = SHOOT
-                break
+    for (enemy_row, enemy_col) in enemies:
+        if ((orientation == 'n' and enemy_col == local_coords.col and enemy_row < local_coords.row) or
+            (orientation == 'e' and enemy_row == local_coords.row and enemy_col > local_coords.col) or
+            (orientation == 's' and enemy_col == local_coords.col and enemy_row > local_coords.row) or
+            (orientation == 'w' and enemy_row == local_coords.row and enemy_col < local_coords.col)):
+            return commandToReturn(Actions.shoot, orientation, in_front)
 
-    if not action:
-        if in_front == FOOD:
-            action = MOVE
-        elif in_front == 'wood-barrier':
-            action = SHOOT
-        elif FOOD in next_to:
-            action = TURN
+    # next see if we're about to be blazed
 
-            dir_to_food = 0
-            while next_to[dir_to_food] != FOOD:
-                dir_to_food = dir_to_food + 1
 
-            if dir_to_food == (current_orientation + 1) % 4:
-                direction = 'right'
-            elif dir_to_food == (current_orientation - 1) % 4:
-                direction = 'left'
-            else:
-                direction = 'about-face'
+    if in_front == Items.food:
+        return commandToReturn(Actions.move, orientation, in_front)
+    elif Items.food in next_to:
+        dir_to_food = 0
+        while next_to[dir_to_food] != Items.food:
+            dir_to_food = dir_to_food + 1
 
-        elif next_to[current_orientation] != 'open':
-            action = TURN
-            direction = random.choice(turn_directions)
+        if dir_to_food == (current_orientation + 1) % 4:
+            direction = TurnDirections.right
+        elif dir_to_food == (current_orientation - 1) % 4:
+            direction = TurnDirections.left
         else:
-            move_or_turn = [MOVE, TURN]
-            action = random.choice(move_or_turn)
+            direction = TurnDirections.behind
 
-            if action == TURN:
-                direction = random.choice(turn_directions)
+        return commandToReturn(Actions.turn, orientation, in_front, direction)
+    elif in_front == Items.wood_barrier:
+        return commandToReturn(Actions.shoot, orientation, in_front)
+    elif in_front != Items.open:
+        return commandToReturn(Actions.turn, orientation, in_front, TurnDirections.random_direction())
+    else:
+        move_or_turn = [Actions.move, Actions.turn]
+        action = random.choice(move_or_turn)
 
-
-    if action == TURN:
-        metadata['direction'] = direction
-
-    save_state = {'coords': local_coords,
-                  'hp': hp,
-                  'prev_action': action,
-                  'prev_orientation': orientation,
-                  'in_front': in_front
-                  }
-
-    command = { 'command': {
-        'action': action,
-        'metadata': metadata,
-        },
-        'state': save_state
-    }
+        if action == Actions.move:
+            return commandToReturn(action, orientation, in_front)
+        elif action == Actions.turn:
+            return commandToReturn(action, orientation, in_front, TurnDirections.random_direction())
 
 
-    return command
+
